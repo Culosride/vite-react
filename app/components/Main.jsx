aa647d50c81109c5bed2ec4c419bb4b6480fe37b
Andrea Spiga <drespriz@gmail.com>
2022-08-11 19:28:58 +0200

    adds main content

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,5 +1,14 @@
 export default function MainContent() {
   return (
-    <h1>ciao</h1>
+    <div className="wrapper">
+      <h1>Random title</h1>
+      <ul className="list">
+        <li>Filler 1</li>
+        <li>Filler 2</li>
+        <li>Filler 3</li>
+        <li>Filler 4</li>
+        <li>Filler 5</li>
+      </ul>
+    </div>
   )
 }
