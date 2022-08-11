f839e93e28933bdc9e561a256d98ba95261fe962
Andrea Spiga <drespriz@gmail.com>
2022-08-11 17:39:08 +0200

    initial commit

---
+++ app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainContent from '../app/components/MainContent.jsx'
+import Navbar from '../app/components/Navbar.jsx'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <MainContent />
+    </>
+  )
+}
---
+++ app/components/MainContent.jsx
@@ -0,0 +1,5 @@
+export default function MainContent() {
+  return (
+    <h1>ciao</h1>
+  )
+}
